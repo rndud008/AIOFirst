fa013129f503b2a951423421a6c43205067ee614
rndud008 <rndud008@gamil.com>
2024-10-09 20:54:33 +0900

    first commit

---
+++ src/main/java/hello/aiofirst/AioFirstApplication.java
@@ -0,0 +1,13 @@
+package hello.aiofirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AioFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AioFirstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/aiofirst/AioFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.aiofirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AioFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
